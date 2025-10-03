448a642e62263825d86770a86be2afd02736a680
Todd Pihl <todd.pihl@gmail.com>
2025-10-03 14:32:23 -0400

    Solved enum problem

---
+++ CIDC2MDF.py
@@ -0,0 +1,203 @@
+# https://github.com/CBIIT/bento-meta/blob/master/python/src/bento_meta/model.py
+import pandas as pd
+import numpy as np
+import argparse
+from crdclib import crdclib
+from bento_meta.model import Model, Node, Property, Term, Edge
+from bento_mdf import MDFWriter
+import yaml
+import json
+
+
+
+def cleanColumnNames(oldnamelist):
+    # The column names have a .1 appended and that causes trouble, so removing
+    newcolumnnames = {}
+    for oldname in oldnamelist:
+        newname = oldname.split('.')[0]
+        newcolumnnames[oldname] = newname
+    return newcolumnnames
+
+
+def cleanEnums(startlist):
+    # The PV list in the spreadsheet is an unholy mess of characters.  Removing offenders and leading/trailing spaces
+    cleaned = None
+    if startlist is not None:
+        removelist = ["'", "[", "]", '"']
+        cleaned = []
+        for entry in startlist:
+            for remove in removelist:
+                entry = entry.replace(remove, '')
+            entry = entry.strip()
+            cleaned.append(entry)
+    return cleaned
+
+def addNodes(datamodel, nodelist):
+    # Add a list of nodes to the model
+    for node in nodelist:
+        nodeobj = Node({'handle': node})
+        datamodel.add_node(nodeobj)
+    return datamodel
+
+def addEnums(propname, df):
+    # Returns a list of permissible values
+    enumlist = None
+    enum_df = df.query('Property == @propname')
+    for index, row in enum_df.iterrows():
+        if row['Permissible Value'] not in ['-', np.nan]:
+            enumlist = row['Permissible Value'].split(',')
+    enumlist = cleanEnums(enumlist)
+    return enumlist
+
+
+
+def addProp(datamodel, df, nodelist):
+    # Adds properties to the model.  Will add PVs if present
+    for nodename in nodelist:
+        working_df = df.query('Node == @nodename')
+        for index, row in working_df.iterrows():
+            propname = row['Property']
+            datatype = row['Data Type']
+            nodes = datamodel.nodes
+            existing = list(nodes[nodename].props.keys())
+            #Only add a new property if it's not there
+            if propname not in existing:
+                enumset = addEnums(propname, working_df)
+                nodeobj = datamodel.nodes[nodename]
+                if enumset is not None:
+                    propdict = {'handle': propname, "_parent_handle": nodename, 'is_required': 'No', 'value_domain': 'value_set'}
+                else:
+                    propdict = {'handle': propname, "_parent_handle": nodename, 'is_required': 'No', 'value_domain': datatype}
+                propobj = Property(propdict)
+                
+                datamodel.add_prop(nodeobj, propobj)
+                if enumset is not None:
+                    workingprop = datamodel.props[(nodename, propname)]
+                    datamodel.add_terms(workingprop, *enumset)
+    return datamodel
+
+def addEnum(datamodel, df):
+    # Not being used
+    proplist = datamodel.props
+    for prop in proplist:
+        print(prop)
+        nodename = prop[0]
+        propname = prop[1]
+        temp_df = df.query('Property == @propname')
+        for index, row in temp_df.iterrows():
+            if row['Permissible Value'] not in ['-', np.nan]:
+                messylist = row['Permissible Value'].split(',')
+                addprop = datamodel.props[(nodename,propname)]
+                datamodel.add_terms(addprop, *messylist)
+    return datamodel
+
+
+
+def addTerm(datamodel, df):
+    # Adds terms to a Property.  Currently not used
+    proplist = df['Property'].unique()
+    # This is a stupid safety valve because some rows are multi-mapped and therefore terms are duplicated.
+    checkit = []
+    for propname in proplist:
+        term_df = df.query('Property == @propname')
+        for index, row in term_df.iterrows():
+            nodename = row['Node']
+            cdename = 'NA'
+            cdever = 'NA'
+            cdeid = 'NA'
+            description = 'None'
+            cdeurl = 'None'
+            if row['Permissible Value'] not in ['-', np.nan]:
+                #print(f"PV:\t {row['Permissible Value']}")
+                #print(f"Type:\t{type(row['Permissible Value'])}")
+                # These are an unholy mess, only thing to do is split on a comma
+                messylist = row['Permissible Value'].split(',')
+                termvalues = {'handle': propname, 'Enum': messylist}
+                termobj = Term(termvalues)
+                propobj = datamodel.props[(nodename, propname)]
+                datamodel.add_terms(propobj, termobj)
+
+                #for entry in messylist:
+                    #print(f"Node:\t{nodename}\tProperty:\t{propname}\tEntry:\t{entry}")
+                    #print(f"Checkit:\t {checkit}")
+                '''
+                    if entry not in checkit:
+                        # OK, Annotate is used to add Term sections.  Which is where the code below came from.  But that's not what populates Enum.  datamodel.add_term?
+                        #termvalues = {'handle': entry, 'value':entry, 'origin_version':cdever, 'origin_name':'CIDC', 'origin_id':cdeid, 'origin_definition': description, 'nanoid': cdeurl}
+                        termvalues = {'handle': entry, 'value': entry}
+                        termobj = Term(termvalues)
+                        propobj = datamodel.props[(nodename, propname)]
+                        #datamodel.annotate(propobj, termobj)
+                        datamodel.add_terms(propobj, termobj)
+                        checkit.append(entry)'''
+    return datamodel
+
+
+
+def writeFiles(mdf, configs):
+    # This writes out the separate mode, property, etc., if reuested in the config.
+    jsonobj = json.dumps(MDFWriter(mdf).mdf)
+    mdfdict = json.loads(jsonobj)
+
+    for entry in configs['mdffiles']:
+        for mdfsection, filename in entry.items():
+            printnode = {}
+            printnode[mdfsection] = mdfdict.pop(mdfsection, None)
+            crdclib.writeYAML(configs['workingpath']+filename, printnode)
+    #After printing out the requested sections, print out what's left
+    crdclib.writeYAML(configs['workingpath']+configs['mdffile'], mdfdict)
+
+
+
+
+
+def main(args):
+    # Read the config file
+    configs = crdclib.readYAML(args.configfile)
+    # Create a dataframe from the Excel sheet
+    cidc_df = pd.read_excel(configs['workingpath']+configs['excelfile'], sheet_name=configs['worksheet'], usecols="G:K", header=8)
+
+    # The header names are problematic, rename
+    newcols = cleanColumnNames(cidc_df.columns.to_list())
+    cidc_df.rename(columns=newcols, inplace=True)
+
+    # Get the list of nodes
+    cidc_nodes = cidc_df['Node'].unique().tolist()
+    # Clean out a couple of bad values
+    cidc_nodes.remove('-')
+    cidc_nodes.remove(np.nan)
+
+
+    # Create an empty model
+    cidc_mdf = Model(handle='CIDC', version='0.01')
+
+    # Add nodes
+    cidc_mdf = addNodes(cidc_mdf, cidc_nodes)
+    # Add properties
+    cidc_mdf = addProp(cidc_mdf, cidc_df, cidc_nodes)
+    # Add terms
+    #cidc_df = addTerm(cidc_mdf, cidc_df)
+    # Add Enums:
+    # cidc_mdf = addEnum(cidc_mdf, cidc_df)
+
+    #print(cidc_mdf.nodes.keys())
+    #print(cidc_mdf.props.keys())
+
+    #Attempt to write da model
+    if configs['separate_files']:
+        writeFiles(cidc_mdf, configs)
+    else:
+        filename = configs['workingpath']+configs['mdffile']
+        with open(filename, "w") as f:
+            f.write(yaml.dump(MDFWriter(cidc_mdf).mdf, indent=4))
+        f.close()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--configfile", required=True,  help="Configuration file containing all the input info")
+    parser.add_argument('-v', '--verbose', action='count', default=0, help=("Verbosity: -v main section -vv subroutine messages -vvv data returned shown"))
+
+    args = parser.parse_args()
+
+    main(args)
+
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # CIDCModel
 Work on CIDC model
+# Scripts
+## CIDC2MDF.py
+A script that reads an Excel spreadsheet of the CIDC model and writes out the MDF compliant file(s).  Requires a YAML configuration file\
+### Usage
+python CIDC2MDF -c /<configfile/> -v /<verbose output/>\
+### Config file options
+ - *workingpath* (String): Path to where MDF files will be written and input Excel files is saved
+ - *excelfile* (String): The name of the input Excel file
+ - *worksheet* (String): The worksheet name containing the nodes, properties, PVs, etc.
+ - *mdffile* (String): Name of the output model file.  This will contain any remaining information if separate files are used.
+ - *separate_files* (Boolean): If True, separate files will be writting for each node listed in *mdffiles*.  If False, all output will be to *mdffile*
+ - *mdffiles* (List of Dictionary):  This is a list of dictionaries with the MDF Section as the key, and the file name as the value.  Valid keys include PropDefiintions, Term, Relationsihps, Terms, Nodes, Handle, Version, Tags.  Any MDF sections not specified here will be printed out to the file specified in *mdffile*\
+ *Example*: (PropDefinitions: 'My_model_properties.yml') will create a *My_model_properties.yml* file containing all the entries under PropDefinitions, and all remaining MDF sections in hte *mdffile*.
