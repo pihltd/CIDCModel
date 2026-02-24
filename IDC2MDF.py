3c33f85ab3d5e2cd8659f93faa418d9de035a193
Todd Pihl <todd.pihl@gmail.com>
2026-02-24 16:24:59 -0500

    Script updates

--- IDC2MDF.py
+++ IDC2MDF.py
@@ -2,7 +2,7 @@ from bento_mdf import MDFWriter
 from bento_meta.model import Model
 import argparse
 import pandas as pd
-from crdclib import crdclib
+#from crdclib import crdclib
 import requests
 import json
 
@@ -11,6 +11,10 @@ from bento_mdf.validator import MDFValidator
 from jsonschema import SchemaError, ValidationError
 from yaml.parser import ParserError
 from IPython.display import clear_output
+
+import sys
+sys.path.append('../CRDCLib/src')
+from crdclib import crdclib
 
 
 def cleanHTML(inputstring):
@@ -64,7 +68,7 @@ def getCDEInfo(cdeid, version=None, verbose=0):
 
 
 
-def mdfAddProperty(mdfmodel, node_prop_dict, add_node = False):
+'''def mdfAddProperty(mdfmodel, node_prop_dict, add_node = False):
     """Adss property objects to an MDF model object.  If requested, missing nodes will be added
 
     :param mdfmodel: An MDF model object to which nodes will be added
@@ -99,7 +103,7 @@ def mdfAddProperty(mdfmodel, node_prop_dict, add_node = False):
                                 'desc': prop_info['desc']})
             nodeobj = mdfmodel.nodes[node]
             mdfmodel.add_prop(nodeobj, propobj)
-    return mdfmodel
+    return mdfmodel'''
 
 
 
@@ -107,11 +111,10 @@ def mdfAddProperty(mdfmodel, node_prop_dict, add_node = False):
 def addProps(datamodel, nodedict, add_node=False):
     node_prop_dict = {}
     edgelist = []
-    taginfo = []
     for node, workign_df in nodedict.items():
         node_prop_dict[node] = []
         for index, row in workign_df.iterrows():
-            valtype = 'String'
+            valtype = 'string'
             req = 'No'
             if pd.notnull(row['Description']):
                 description = crdclib.cleanString(str(row['Description']), True)
@@ -122,34 +125,13 @@ def addProps(datamodel, nodedict, add_node=False):
                 tempinfo = {}
                 propname = crdclib.cleanString(row['Property'], True)
                 propname = cleanHTML(propname)
-                print(f"Working on property {propname}")
-                if "." in propname:
-                    #print(f"This is a node.propery: {propname} for node {node}")
-                    dstnode = propname.split(".")[0]
-                    dstnode = dstnode.capitalize()
-                    edgeinfo = {
-                        'handle': f"of_{node}",
-                        'desc': 'Obtained from node.property',
-                        'mul': 'Unknown',
-                        'ends':[{'src': node, 'dst': dstnode}]
-                    }
-                    edgelist.append(edgeinfo)
-                else:
-                    if pd.notnull(row['Required/optional']):
-                        if row['Required/optional'] == 'R':
-                            req = 'Yes'
-                    tempinfo = {'prop': propname, "_parent_handle": node, 'isreq': req, 'val': valtype, 'desc': description}
-                    #if "_id" in propname:
-                    if propname.endswith("_id"):
-                        print(f"{propname} is Key")
-                        #tempinfo['Key'] = 'True'
-                        #taginfo.append({'node':node, 'prop':propname, 'tag':{'key':'Key:', 'value': True}})
-                        tempinfo['iskey'] = 'Yes'
-                        print(tempinfo)
-                    node_prop_dict[node].append(tempinfo)
-    datamodel = mdfAddProperty(datamodel, node_prop_dict, False)
-    #for tag in taginfo:
-    #    crdclib.mdfAddTags(datamodel, 'property',(tag['node'],tag['prop']),tag['tag'])
+                if row['Required/optional'] == 'R':
+                    req = 'Yes'
+                tempinfo = {'prop': propname, "_parent_handle": node, 'isreq': req, 'val': valtype, 'desc': description}
+                if row['Key'] == 'yes':
+                    tempinfo['iskey'] = 'True'
+                node_prop_dict[node].append(tempinfo)
+    datamodel = crdclib.mdfAddProperty(datamodel, node_prop_dict, False)
     return datamodel, edgelist
 
 
@@ -157,16 +139,16 @@ def addProps(datamodel, nodedict, add_node=False):
 
 
 def addTerms(datamodel, nodedict, verbose=0):
-    # TODO: If there is no CDE, but a Permissible values column, add those as enums
     for nodename, working_df in nodedict.items():
         for index, row in working_df.iterrows():
             if 'CDE' in row:
                 if pd.notnull(row['CDE']):
-                    #cdeinfo = getCDEInfo(row['CDE'], verbose=verbose)
                     cdeinfo = crdclib.getCDEInfo(row['CDE'])
                     if cdeinfo['cdedef'] is not None:
                         cdedef = crdclib.cleanString(cdeinfo['cdedef'],True)
                         cdedef = cleanHTML(cdedef)
+                    else:
+                        cdedef = None
                     cdeid = str(row['CDE'])
                     # For some reason, IDs out of Excel are formated like a float
                     cdeid = cdeid.split(".")[0]
@@ -174,9 +156,7 @@ def addTerms(datamodel, nodedict, verbose=0):
                     datamodel = crdclib.mdfAnnotateTerms(datamodel, nodename, row['Property'], termvalues)
                 elif 'Permissible values' in row:
                     if pd.notnull(row['Permissible values']):
-                      #print(f"Type: {type(row['Permissible values'])}\n{row['Permissible values']}")
                       pvlist = row['Permissible values'].split("\n")
-                      #print(f"Length of pvlist: {len(pvlist)}")
                       datamodel = crdclib.mdfAddEnums(mdfmodel=datamodel, nodename=nodename, propname=row['Property'], enumlist=pvlist)
     return datamodel
 
@@ -228,6 +208,68 @@ def addTags(datamodel, taglist, verbose=0):
 
 
 
+def mdfBuildLoadSheets(mdf):
+    """Uses an MDF model to build a complete set of load sheets suitable for use in the Submission Portal.  Returns a dictionary of dataframes with the node as the key.  Note that the 'type' column is NOT added by this routine.
+    Note that the node,property field is added to the SRC node of the edges.
+    
+    :param mdf: MDF Model Object
+    :rtype: Dictionary.  Keys are nodes, values are dataframes that can be printed to CSV and used as load sheets
+    """
+
+    loadsheets = {}
+    #reqlist = []
+    nodes = mdf.nodes
+    for node in nodes:
+        nodeprops = mdf.nodes[node].props
+        nodelist = []
+        for prop in nodeprops:
+            if 'Template' in mdf.props[(node, prop)].tags:
+                # Remove any property that is set to 'Template: No'
+                if mdf.props[(node,prop)].tags['Template'].get_attr_dict()['value'] != 'No':
+                    nodelist.append(prop)
+            else:
+                nodelist.append(prop)
+        # Now need to add the relationship columns.  There are usually expressed as node.property
+        # Currnetly the NCI Image model has things reversed, so to get the links on the proper load sheets, edges_by_dst is required.
+        srcedges = mdf.edges_by_src(mdf.nodes[node]) 
+        #srcedges = mdf.edges_by_dst(mdf.nodes[node])
+
+        #print(f"\nEdges for node {node}:")
+        #for srcedge in srcedges:
+        #    print(f"Edge: {srcedge}")
+        for srcedge in srcedges:
+            # Need to find the destination node:
+            # As noted before, NCI Image model has this backwareds, so need to sue srcedge.src.handle.
+            dstnode = srcedge.dst.handle
+            #dstnode = srcedge.src.handle
+
+            #Now get the properties for that node
+            #print(f"Node: {node}\tSrc edge: {srcedge}\t Dst Node: {dstnode}")
+            dstprops = mdf.nodes[dstnode].props
+            #print(f"Dst Prop list: {dstprops}")
+            reqlist = []
+            for dstprop in dstprops:
+                # Relationship columns are based on key columns in the dst noe
+                if 'is_key' in mdf.props[(dstnode, dstprop)].get_attr_dict():
+                    #print(f"is_key value: {mdf.props[(dstnode, dstprop)].get_attr_dict()['is_key']}\t and is type {type(mdf.props[(dstnode, dstprop)].get_attr_dict()['is_key'])}")
+                    #print(f"Property {dstprop} is Key")
+                    if mdf.props[(dstnode, dstprop)].get_attr_dict()['is_key'] == 'True':
+                        #print(f"Adding {dstnode}.{dstprop} to reqlist")
+                        reqlist.append(dstnode+'.'+dstprop)
+                        #reqlist.insert(0, dstnode+'.'+dstprop)
+            #nodelist.extend(reqlist)
+            #print(f"Reqlist: {reqlist}")
+            if len(reqlist) > 0:
+                for entry in reqlist:
+                    nodelist.insert(0, entry)
+        #Add the type column
+        nodelist.insert(0, 'type')
+
+        load_df = pd.DataFrame(columns=nodelist)
+        loadsheets[node] = load_df
+    return loadsheets
+
+
         
 
 def main(args):
@@ -238,7 +280,6 @@ def main(args):
     nodedict = {}
 
     #Read the input file
-    
     if args.verbose >= 1:
         print(f"Reading Excel file {configs['excelfile']}")
     xlfile = pd.ExcelFile(configs['workingpath']+configs['excelfile'])
@@ -247,7 +288,11 @@ def main(args):
     if args.verbose >= 1:
         print("Setting up node/dataframe dictionary")
     for node in xlfile.sheet_names:
-        if node not in configs['excludetabs']:
+        if node == configs['edgesheet']:
+            if args.verbose >= 2:
+                print('Populating edge_df')
+            edge_df = pd.read_excel(configs['workingpath']+configs['excelfile'], node)
+        elif node not in configs['excludetabs']:
             temp_df = pd.read_excel(configs['workingpath']+configs['excelfile'], node)
             nodedict[node] = temp_df
     
@@ -277,26 +322,19 @@ def main(args):
     if 'tags' in configs:
         idc_mdf = addTags(idc_mdf, configs['tags'], args.verbose )
 
-    # Add edges if they are provided in the configs file
-    if 'edges' in configs:
-        if len(configs['edges']) >= 1:
-            if args.verbose >= 1:
-                print("Adding relationships to the model")
-            for edge in configs['edges']:
-                edgelist.append(edge)
-            #idc_mdf = addEdges(idc_mdf, configs['edges'])
-            idc_mdf = addEdges(idc_mdf, edgelist)
-    elif len(edgelist) > 0:
-        if args.verbose >= 1:
-            print("Adding edges to model")
-        idc_mdf = addEdges(idc_mdf, edgelist, args.verbose)
+    # Add edges
+    if args.verbose >= 1:
+        print("Adding edges to model")
+    edgelist = []
+    for index, row in edge_df.iterrows():
+        edgelist.append({
+        'handle': f"of_{row['Destination node']}",
+        'desc': f"Data of {row['Destination node']}",
+        'mul': row['Cardinality'],
+        'ends': [{'src': row['Source node'], 'dst': row['Destination node']}]
+        })
 
-
-
-  #  # Model is done, validated before writing
-  #  if args.verbose >= 1:
-  #      print("Validating final model")
-  #  validateModel(idc_mdf)
+    idc_mdf = addEdges(idc_mdf, edgelist, args.verbose)
 
     # Write out the files
     if args.verbose >= 1:
@@ -310,6 +348,15 @@ def main(args):
         for filename in fileentry.values():
             filelist.append(f"{configs['workingpath']}{filename}")
     validateModel(filelist)
+
+    if configs['loadsheetpath'] is not None:
+        if args.verbose >= 1:
+            print(f"Writing data load sheets in {configs['loadsheetpath']}")
+        load_df = crdclib.mdfBuildLoadSheets(idc_mdf, reverse=False, typecolumn=True)
+        #load_df = mdfBuildLoadSheets(idc_mdf)
+        for node, loadsheet_df in load_df.items():
+            filename = f"{configs['loadsheetpath']}NCI_Imaging_Data_Loading_Template_{node}.tsv"
+            loadsheet_df.to_csv(filename, sep="\t", index=False)
 
 
 if __name__ == "__main__":
